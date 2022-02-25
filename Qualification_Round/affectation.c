361f5d2a5fc9d83d1444caad38ef267e9059e310
ImadAbid <imad.abied@gmail.com>
2022-02-25 12:30:22 +0100

    bugs fixing

--- Qualification_Round/affectation.c
+++ Qualification_Round/affectation.c
@@ -37,17 +37,17 @@ void affectations_update(int t){
 
         affected = 1;
 
-        affectations[i].project_id = i;
-        affectations[i].contributor_ids_size = project_tab[i].roles_nbr;
-        affectations[i].contributor_ids = malloc(affectations[i].contributor_ids_size * sizeof(int));
+        affectations[affectations_size].project_id = i;
+        affectations[affectations_size].contributor_ids_size = project_tab[i].roles_nbr;
+        affectations[affectations_size].contributor_ids = malloc(affectations[affectations_size].contributor_ids_size * sizeof(int));
         for(int role_i = 0; role_i < project_tab[i].roles_nbr; role_i++){
             contributor_id = skill_masters_get(project_tab[i].roles_id[role_i], project_tab[i].roles_level[role_i], t);
             if(contributor_id == -1){
-                free(affectations[i].contributor_ids);
+                free(affectations[affectations_size].contributor_ids);
                 affected = 0;
                 break;
             }
-            affectations[i].contributor_ids[role_i] = contributor_id;
+            affectations[affectations_size].contributor_ids[role_i] = contributor_id;
             if(contributor_tab[contributor_id].available_after > start_time){
                 start_time = contributor_tab[contributor_id].available_after;
             }
@@ -55,11 +55,15 @@ void affectations_update(int t){
 
         if(!affected) continue;
 
-        affectations[i].start_time = start_time;
+        project_tab[i].affected = 1;
 
-        for(int j = 0; j < affectations[i].contributor_ids_size; j++){
-            contributor_tab[affectations[i].contributor_ids[j]].available_after = affectations[i].start_time + project_tab[affectations[i].project_id].duration;
+        affectations[affectations_size].start_time = start_time;
+
+        for(int j = 0; j < affectations[affectations_size].contributor_ids_size; j++){
+            contributor_tab[affectations[affectations_size].contributor_ids[j]].available_after = affectations[affectations_size].start_time + project_tab[affectations[affectations_size].project_id].duration;
         }
+
+        affectations_size++;
 
     }
 }
