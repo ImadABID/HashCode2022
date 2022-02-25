b41151cb528677086d1249948e285041e48de47c
ImadAbid <imad.abied@gmail.com>
2022-02-25 20:09:59 +0100

    bash script to compute all & zip src

---
+++ Qualification_Round/compute_all_examples.sh
@@ -0,0 +1,7 @@
+make zip
+./main < in/a_an_example.in.txt > out/a.txt
+./main < in/b_better_start_small.in.txt > out/b.txt
+./main < in/c_collaboration.in.txt > out/c.txt
+./main < in/d_dense_schedule.in.txt > out/d.txt
+./main < in/e_exceptional_skills.in.txt > out/e.txt
+./main < in/f_find_great_mentors.in.txt > out/f.txt
